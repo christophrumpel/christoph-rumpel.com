85c2619beda1be2ef73190938e677652de23ad6b
Christoph Rumpel <c.rumpel@kabsi.at>
2020-07-16 17:23:05 +0200

    Fix blognote on mobile shifts border

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,8 +3,8 @@
         <li class="font-display text-2xl px-4"><a class="text-link" href="/">Blog</a></li>
 {{--        <li class="font-display text-2xl px-4"><a href="{{ route('page.products') }}">Products</a></li>--}}
         <li class="font-display text-2xl px-4"><a class="text-link" href="{{ route('page.speaking') }}">Speaking</a></li>
-        <li class="font-display text-2xl px-4"><a class="text-link" href="https://callitaday.transistor.fm">Podcast</a></li>
         <li class="font-display text-2xl px-4"><a class="text-link" href="{{ route('page.uses') }}">Uses</a></li>
+        <li class="font-display text-2xl px-4"><a class="text-link" href="https://callitaday.transistor.fm">Podcast</a></li>
     </ul>
 </nav>
 
