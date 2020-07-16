c8199e7752d93109e570d2458163d591cf09e587
Christoph Rumpel <c.rumpel@kabsi.at>
2020-07-16 18:28:28 +0200

    Update home meta

--- resources/views/partials/metaHome.blade.php
+++ resources/views/partials/metaHome.blade.php
@@ -1,11 +1,11 @@
 <meta property="og:url" content="{{ getenv('APP_URL') }}"/>
 <meta property="og:type" content="article"/>
 <meta property="og:title" content="Christoph Rumpel"/>
-<meta property="og:description" content="Being able to talk about code and my business has become a valuable skill for me and I wouldn't want to miss it."/>
+<meta property="og:description" content="Welcome to my site, where I share my blog posts and products with you. I hope you will find what you were looking for 😀"/>
 <meta property="og:image" content="{{ asset('images/twitter_preview_larage_home.png') }}"/>
 
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="@christophrumpel" />
 <meta name="twitter:title" content="Christoph Rumpel" />
-<meta name="twitter:description" content="Being able to talk about code and my business has become a valuable skill for me and I wouldn't want to miss it." />
+<meta name="twitter:description" content="Welcome to my site, where I share my blog posts and products with you. I hope you will find what you were looking for 😀" />
 <meta name="twitter:image" content="{{ asset('images/twitter_preview_larage_home.png') }}" />
