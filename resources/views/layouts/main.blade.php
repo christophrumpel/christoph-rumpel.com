74cfd00fc591642ee98d15b4a8282d8b14452a79
Christoph Rumpel <c.rumpel@kabsi.at>
2020-07-17 17:31:54 +0200

    Inline google font styles

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,8 +14,8 @@
 
 <!-- Styles -->
     <link href="{{ asset('css/main.css') }}" rel="stylesheet"/>
-    <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;0,700;1,400&display=swap"
-          rel="stylesheet">
+{{--    <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;0,700;1,400&display=swap"--}}
+{{--          rel="stylesheet">--}}
     @livewireStyles
 
 @if($page === 'home')
@@ -35,6 +35,45 @@
     @endproduction
 
     <script defer src="{{ mix('js/app.js') }}"></script>
+
+    <style>
+        /* latin-ext */
+        @font-face {
+            font-family: 'Bitter';
+            font-style: normal;
+            font-weight: 400;
+            font-display: swap;
+            src: local('Bitter Regular'), local('Bitter-Regular'), url(https://fonts.gstatic.com/s/bitter/v15/rax8HiqOu8IVPmn7cYxpLjpSm3LZ.woff2) format('woff2');
+            unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+        }
+        /* latin */
+        @font-face {
+            font-family: 'Bitter';
+            font-style: normal;
+            font-weight: 400;
+            font-display: swap;
+            src: local('Bitter Regular'), local('Bitter-Regular'), url(https://fonts.gstatic.com/s/bitter/v15/rax8HiqOu8IVPmn7f4xpLjpSmw.woff2) format('woff2');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
+        /* latin-ext */
+        @font-face {
+            font-family: 'Bitter';
+            font-style: normal;
+            font-weight: 700;
+            font-display: swap;
+            src: local('Bitter Bold'), local('Bitter-Bold'), url(https://fonts.gstatic.com/s/bitter/v15/rax_HiqOu8IVPmnzxKl8DRhfsUjQ8Qad.woff2) format('woff2');
+            unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+        }
+        /* latin */
+        @font-face {
+            font-family: 'Bitter';
+            font-style: normal;
+            font-weight: 700;
+            font-display: swap;
+            src: local('Bitter Bold'), local('Bitter-Bold'), url(https://fonts.gstatic.com/s/bitter/v15/rax_HiqOu8IVPmnzxKl8AxhfsUjQ8Q.woff2) format('woff2');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        }
+    </style>
 
 </head>
 
