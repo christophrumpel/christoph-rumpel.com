d26f516cf62b7a9d4a8f78ef1ef9535c38214143
Christoph Rumpel <c.rumpel@kabsi.at>
2020-08-23 16:36:49 +0200

    Add mail notification to backup to fix bug

--- config/backup.php
+++ config/backup.php
@@ -121,12 +121,12 @@ return [
     'notifications' => [
 
         'notifications' => [
-            //\Spatie\Backup\Notifications\Notifications\BackupHasFailed::class => ['mail'],
-            //\Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFound::class => ['mail'],
-            //\Spatie\Backup\Notifications\Notifications\CleanupHasFailed::class => ['mail'],
-            //\Spatie\Backup\Notifications\Notifications\BackupWasSuccessful::class => ['mail'],
-            //\Spatie\Backup\Notifications\Notifications\HealthyBackupWasFound::class => ['mail'],
-            //\Spatie\Backup\Notifications\Notifications\CleanupWasSuccessful::class => ['mail'],
+            \Spatie\Backup\Notifications\Notifications\BackupHasFailed::class => ['mail'],
+            \Spatie\Backup\Notifications\Notifications\UnhealthyBackupWasFound::class => ['mail'],
+            \Spatie\Backup\Notifications\Notifications\CleanupHasFailed::class => ['mail'],
+            \Spatie\Backup\Notifications\Notifications\BackupWasSuccessful::class => ['mail'],
+            \Spatie\Backup\Notifications\Notifications\HealthyBackupWasFound::class => ['mail'],
+            \Spatie\Backup\Notifications\Notifications\CleanupWasSuccessful::class => ['mail'],
         ],
 
         /*
@@ -136,7 +136,7 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'your@example.com',
+            'to' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
