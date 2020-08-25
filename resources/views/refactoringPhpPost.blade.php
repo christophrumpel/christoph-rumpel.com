f47c3357be33a2a4ce30a86eb1c8728c047d7057
Christoph Rumpel <c.rumpel@kabsi.at>
2020-08-25 16:49:55 +0200

    wip

--- resources/views/refactoringPhpPost.blade.php
+++ resources/views/refactoringPhpPost.blade.php
@@ -3,13 +3,15 @@
 <p>This might be an easy tip, but giving good explicit names to variables and methods will improve your code a lot. But
     then developers also say <code>naming</code> is one of the hardest things in programming. 🤷‍</p>
 
-<h3>Example #1</h3>
-
+<h3>Example #1 - Naming</h3>
 <x-code-tab code-name="explicit-code-example-1"></x-code-tab>
 
-<h3>Example #2</h3>
-
+<h3>Example #2 - Naming</h3>
 <x-code-tab code-name="explicit-code-example-2"></x-code-tab>
+
+<h3>Example #3 - Extracting</h3>
+<x-code-tab code-name="explicit-code-example-3"></x-code-tab>
+
 
 <h2>Early Returns</h2>
 
