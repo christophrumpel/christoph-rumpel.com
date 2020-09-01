6a0c50d86580667ccadb0cfd7b557a8b52ada3db
Christoph Rumpel <c.rumpel@kabsi.at>
2020-09-01 17:36:18 +0200

    Fix typos

--- resources/views/refactoringPhpPost.blade.php
+++ resources/views/refactoringPhpPost.blade.php
@@ -1,21 +1,20 @@
 <p class="text-xl lg:text-2xl p-4 lg:p-8 my-8 italic rounded-lg bg-blue-100 border-b-2 border-highlightBlue">
-    I've been programming in PHP now for almost 10 years and if there is one thing I learned over this period, it's that
+    I've been programming in PHP now for almost ten years, and if there is one thing I learned over this period, it's that
     <b>readability</b> and <b>simplicity</b> are the keys for maintainable and sustainable code. Every first attempt to write code should be about <b>making it work</b>. Only after that, you should take some time to refactor. This is when I aim for readability and simplicity. Today I see refactoring as one of my main skills. In this post, I share with you my refactoring practices for PHP.
 </p>
 
 <p>Refactoring is the process of modifying and restructuring code without changing its functionality. When I first heard
     about it, I was like:
-    <code>Why would anyone do that?</code> It took some years until I fully understand the concept and that working code
-    is not always good code too. So with refactoring you are able to make working code better, and what better means is something developers will argue for hours. In the end, you have to define it for yourself.</p>
+    <code>Why would anyone do that?</code> It took some years until I fully understood the concept and that the working code is not always good. So with refactoring, you can make working code better, and what better means is something developers will argue for hours. In the end, you have to define it for yourself.</p>
 
 <p>In the following examples, I will show you how I define better code.</p>
 
 <h2>#1 - Be Expressive</h2>
 
-<p>This might be an easy tip, but writing expressive code can improve it a lot. Always try to make your code self-explaining so that you, your future self or any other developers who stumbles over your code knows what is going on.</p>
+<p>This might be an easy tip, but writing expressive code can improve it a lot. Always make your code self-explaining so that you, your future self or any other developers who stumble over your code knows what is going on.</p>
 
 <p>But then developers also say <code>naming
-    is one of the hardest things in programming.</code> This is one reason why this is not as easy as it sounds. 🤷‍</p>
+        is one of the hardest things in programming.</code> This is one reason why this is not as easy as it sounds. 🤷‍</p>
 
 <div class="blognote"><strong>Note:</strong> Make sure to hit the "Show/Hide Comments" buttons on the code examples to get
     details about why we needed to chang the code.
@@ -36,9 +35,7 @@
 
 <h2>#2 - Return Early</h2>
 
-<p>The concept of <code>early returns</code> refers to a practice where we try to avoid nesting by
-    breaking a structure down to specific cases. In return, we will get a more linear code which is
-    much easier to read and grasp. Every case is separated and good to follow. Don't be afraid of using
+<p>The concept of <code>early returns</code> refers to a practice where we try to avoid nesting by breaking a structure down to specific cases. In return, we will get a more linear code, which is much easier to read and grasp. Every case is separated and good to follow. Don't be afraid of using
     multiple return statements.</p>
 
 <h3>Example #1</h3>
@@ -51,9 +48,9 @@
 
 <h2>#3 - Refactor To Collections</h2>
 
-<p>In PHP, we work a lot with arrays of different data. The available features to handle and transform those arrays are quite limited in PHP and don't provide a good experience while using them. (array_walk, usort, etc.)</p>
+<p>In PHP, we work a lot with arrays of different data. The available features to handle and transform those arrays are quite limited in PHP and don't provide a good experience. (array_walk, usort, etc.)</p>
 
-<p>To tackle this problem there is this concept of Collection classes, which help you to handle arrays. Most known is the <a href="https://laravel.com/docs/master/collections">implementation in Laravel</a>, where a collection class provides you with dozens of useful features to work with arrays.</p>
+<p>To tackle this problem, there is this concept of Collection classes, which help you handle arrays. Most known is the <a href="https://laravel.com/docs/master/collections">implementation in Laravel</a>, where a collection class provides you with dozens of useful features to work with arrays.</p>
 
 <div class="blognote"><strong>Note:</strong> For the following examples, I will use Laravel's collect() helper, but the approach is very similar with other available collection classes from other frameworks or libraries.</div>
 
