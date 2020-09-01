aff220f053c8af7150fc3b21cf3d4ce8c766377a
Christoph Rumpel <c.rumpel@kabsi.at>
2020-09-01 10:45:23 +0200

    wip

--- app/Http/Controllers/PagePostController.php
+++ app/Http/Controllers/PagePostController.php
@@ -4,12 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Post\PostCollector;
 use Illuminate\Http\Request;
-use League\CommonMark\Block\Element\FencedCode;
-use League\CommonMark\Block\Element\IndentedCode;
-use League\CommonMark\CommonMarkConverter;
-use League\CommonMark\Environment;
-use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
-use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
 
 class PagePostController extends Controller
 {
@@ -21,12 +15,6 @@ class PagePostController extends Controller
             abort(404);
         }
 
-        $environment = Environment::createCommonMarkEnvironment();
-        $environment->addBlockRenderer(FencedCode::class, new FencedCodeRenderer());
-        $environment->addBlockRenderer(IndentedCode::class, new IndentedCodeRenderer());
-
-        $commonMarkConverter = new CommonMarkConverter([], $environment);
-
-        return view('pages.post', ['post' => $post, 'markdownConverter' => $commonMarkConverter]);
+        return view('pages.post', ['post' => $post]);
     }
 }
