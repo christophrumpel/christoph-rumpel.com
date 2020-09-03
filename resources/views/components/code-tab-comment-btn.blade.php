8cdb2bbd0c9b2da8cfe72759b3d135e6a41a9afa
Christoph Rumpel <c.rumpel@kabsi.at>
2020-09-03 15:34:32 +0200

    Use Blade UI Kit to add icon to hide/show notes

--- resources/views/components/code-tab-comment-btn.blade.php
+++ resources/views/components/code-tab-comment-btn.blade.php
@@ -1,5 +1,6 @@
 <button
     @click.prevent="tab = '{{ $tabState }}'"
-    class="text-bgBlue absolute right-0 text-sm block px-4 py-4 underline uppercase"
->{{ $btnText }}
+    class="flex flex-column text-bgBlue absolute right-0 text-sm block px-4 py-4 underline uppercase"
+>
+    <x-far-lightbulb class="h-5 w-5 mr-2 text-highlightYellow" /> {{ $btnText }}
 </button>
