0f233310d58c0533c70d22a389540169cb9c25be
Christoph Rumpel <c.rumpel@kabsi.at>
2020-09-04 11:05:11 +0200

    Add anchor links to refactoring article

--- resources/views/refactoringPhpPost.blade.php
+++ resources/views/refactoringPhpPost.blade.php
@@ -2,9 +2,15 @@
     about it, I was like:
     <code>Why would anyone do that?</code> It took some years until I fully understood the concept and that the working code is not always good. So with refactoring, you can make working code better, and what better means is something developers will argue for hours. In the end, you have to define it for yourself.</p>
 
-<p>In the following examples, I will show you how I define better code.</p>
+<p>In the following examples, I will show you how I define better code. Are you just looking for a specific strategy? Please use the <code>anchor links</code>:</p>
+<ul>
+    <li><a href="#be-expressive">#1 - Be Expressive</a></li>
+    <li><a href="#return-early">#2 - Return Early</a></li>
+    <li><a href="#refactor-to-collections">#3 - Refactor To Collections</a></li>
+    <li><a href="#consistency">#4 - Consistency</a></li>
+</ul>
 
-<h2>#1 - Be Expressive</h2>
+<h2 id="be-expressive">#1 - Be Expressive</h2>
 
 <p>This might be an easy tip, but writing expressive code can improve it a lot. Always make your code self-explaining so that you, your future self or any other developers who stumble over your code knows what is going on.</p>
 
@@ -28,7 +34,7 @@
 <x-code-tab code-name="expressive-code-example-4"></x-code-tab>
 
 
-<h2>#2 - Return Early</h2>
+<h2 id="return-early">#2 - Return Early</h2>
 
 <p>The concept of <code>early returns</code> refers to a practice where we try to avoid nesting by breaking a structure down to specific cases. In return, we will get a more linear code, which is much easier to read and grasp. Every case is separated and good to follow. Don't be afraid of using
     multiple return statements.</p>
@@ -41,7 +47,7 @@
 
 <div class="blognote"><strong>Note:</strong> You sometimes also hear the term "guard-clauses" which is what you can achieve with early returns. You are guarding your method from special conditions.</div>
 
-<h2>#3 - Refactor To Collections</h2>
+<h2 id="refactor-to-collections">#3 - Refactor To Collections</h2>
 
 <p>In PHP, we work a lot with arrays of different data. The available features to handle and transform those arrays are quite limited in PHP and don't provide a good experience. (array_walk, usort, etc.)</p>
 
@@ -58,7 +64,7 @@
 <div class="blognote"><strong>Note:</strong> Adam Wathan wrote a <a href="https://adamwathan.me/refactoring-to-collections/">book</a>
  about how to use collections to never write a loop again which I can highly recommend.</div>
 
-<h2>Consistency</h2>
+<h2 id="consistency">#4 - Consistency</h2>
 
 <p>Every line of code adds a little amount of visual noise. The more code, the more difficult it gets to read. This is why it is essential to set rules. Keeping similar things consistent will help you recognize code and patterns. It will result in less noise and more readable code.</p>
 
