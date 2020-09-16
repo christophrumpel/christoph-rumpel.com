3deff33bda311ccbfa9280d5e70f303adf68fdae
Christoph Rumpel <c.rumpel@kabsi.at>
2020-09-16 07:51:51 +0200

    Fix typo

--- config/secure-headers.php
+++ config/secure-headers.php
@@ -79,7 +79,7 @@ return [
      *                  'same-origin', 'strict-origin', 'strict-origin-when-cross-origin', 'unsafe-url'
      */
 
-    'referrer-policy' => 'origin^',
+    'referrer-policy' => 'origin',
 
     /*
      * Clear-Site-Data
