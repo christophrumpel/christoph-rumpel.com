7c52b686706881e39d223cc02a84e44241c06744
Christoph Rumpel <c.rumpel@kabsi.at>
2020-12-21 13:28:17 +0100

    Add auth to api and add user token

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,12 @@ use Spatie\Mailcoach\Models\Subscriber;
 |
 */
 
-Route::middleware('api')->get('/newsletter/mastering-phpstorm/count', function (Request $request) {
+Route::middleware('auth:api')->get('/newsletter/mastering-phpstorm/count', function (Request $request) {
     $emailList = EmailList::findByUuid(getenv('MAILCOACH_MASTERING_PHPSTORM_LIST_UUID'));
     return response()->json(['count' => $emailList->subscribers()->count()]);
 });
 
-Route::middleware('api')->post('/newsletter/mastering-phpstorm/subscribe', function (Request $request) {
+Route::middleware('auth:api')->post('/newsletter/mastering-phpstorm/subscribe', function (Request $request) {
 
     $attributes = $request->only(['email', 'tags']);
     $emailList = EmailList::findByUuid(getenv('MAILCOACH_MASTERING_PHPSTORM_LIST_UUID'));
