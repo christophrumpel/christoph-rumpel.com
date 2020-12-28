87ad71b33059542d75fb6139fc4f5c98d3c6e961
christophrumpel <christophrumpel@users.noreply.github.com>
2020-12-28 15:13:11 +0000

    Apply php-cs-fixer changes

--- app/Http/Controllers/PagePrivacyMpPolicyController.php
+++ app/Http/Controllers/PagePrivacyMpPolicyController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 class PagePrivacyMpPolicyController extends Controller
 {
-
     public function __invoke(Request $request)
     {
         return view('pages.privacy-policy-mp');
