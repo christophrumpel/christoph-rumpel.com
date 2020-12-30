236fe88c4a54ca14ea332261b46ce44174e2fbd4
Christoph Rumpel <c.rumpel@kabsi.at>
2020-12-30 09:51:45 +0100

    Add video and slide links to talks and add basic article

--- app/Http/Controllers/PageSpeakingController.php
+++ app/Http/Controllers/PageSpeakingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class PageSpeakingController extends Controller
 {
@@ -11,6 +12,31 @@ class PageSpeakingController extends Controller
     {
         $talks = json_decode(Storage::disk('talks')->get('talks.json'));
 
+        $talks = $this->preparePastTalksDetails($talks);
+
         return view('pages.speaking', ['talks' => $talks]);
+    }
+
+    private function preparePastTalksDetails($talks)
+    {
+        $pastTalks = collect($talks->past)->transform(function($talk){
+            $details = Str::of($talk->location);
+
+            if(isset($talk->slides)) {
+                $details = $details->append(', <a href="'.$talk->slides.'">Slides</a>');
+            }
+
+            if(isset($talk->video)) {
+                $details = $details->append(", <a href='$talk->video'>Video</a>");
+            }
+
+            $details = $details->prepend('(')
+                ->append(')');
+
+
+            return tap($talk, fn($talk) => $talk->details = $details);
+        });
+
+        return tap($talks, fn($talks) => $talks->past = $pastTalks);
     }
 }
--- resources/views/pages/speaking.blade.php
+++ resources/views/pages/speaking.blade.php
@@ -5,9 +5,13 @@
 
         <img src="{{ asset('images/pages/speaking.jpg') }}" alt="Christoph speaking at a conference.">
 
-        <p>In 2015, I gave my first talk ever, and it was terrifying! I will never forget how nervous I was and how terrible I felt. But I also remember the feeling afterward; I felt relieved, proud, and got in touch with some attendees to discuss my topic.</p>
+        <p>In 2015, I gave my first talk ever, and it was terrifying! I will never forget how nervous I was and how
+            terrible I felt. But I also remember the feeling afterward; I felt relieved, proud, and got in touch with
+            some attendees to discuss my topic.</p>
         <p>
-            I kept on doing talks, and some years later, I eventually was able to speak at international conferences and also get paid. Being able to talk about code and my business has become a valuable skill for me and I wouldn't want to miss it.</p>
+            I kept on doing talks, and some years later, I eventually was able to speak at international conferences and
+            also get paid. Being able to talk about code and my business has become a valuable skill for me and I
+            wouldn't want to miss it.</p>
 
         <p>Currently I speak about:</p>
         <ul>
@@ -17,7 +21,9 @@
             <li><code>My one-person business</code></li>
         </ul>
 
-        <p>If you would like to have me on your conference or meetup, <a class="text-link" href="https://twitter.com/christophrumpel/">please contact me on Twitter</a>.</p>
+        <p>If you would like to have me on your conference or meetup, <a class="text-link"
+                                                                         href="https://twitter.com/christophrumpel/">please
+                contact me on Twitter</a>.</p>
 
         <h2>Upcoming</h2>
         @if(count($talks->upcoming))
@@ -44,7 +50,7 @@
             @foreach($talks->past as $talk)
                 <li>
                     <h3>{{ $talk->date }} - {{ $talk->event }}</h3>
-                    <p>{{ $talk->title }} ({{ $talk->location }})</p>
+                    <p>{{ $talk->title }} {!! $talk->details !!}</p>
                 </li>
             @endforeach
         </ul>
