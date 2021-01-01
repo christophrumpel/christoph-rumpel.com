b3f7191fe014306576ba1511b2e8e502a36136ec
Christoph Rumpel <c.rumpel@kabsi.at>
2021-01-01 15:15:50 +0100

    Add missing media library db field

---
+++ database/migrations/2021_01_01_144533_add_generated_conversions_to_media_library.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddGeneratedConversionsToMediaLibrary extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->json('generated_conversions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            //
+        });
+    }
+}
