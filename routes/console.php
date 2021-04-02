0f36ae30d39bfbfce3d062f342c6068039a2b7ba
Christoph Rumpel <c.rumpel@kabsi.at>
2021-04-02 14:39:47 +0200

    Add segment ugprade command for mailcoach

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+use Spatie\Mailcoach\Domain\Campaign\Models\Campaign;
+
+Artisan::command('migrate-mailcoach', function () {
+    Campaign::each(function (Campaign $campaign) {
+        if ($campaign->segment_class === 'Spatie\Mailcoach\Support\Segments\SubscribersWithTagsSegment') {
+            $campaign->update([
+                'segment_class' => 'Spatie\Mailcoach\Domain\Audience\Support\Segments\SubscribersWithTagsSegment',
+            ]);
+        }
+
+        if ($campaign->segment_class === 'Spatie\Mailcoach\Support\Segments\EverySubscriberSegment') {
+            $campaign->update([
+                'segment_class' => 'Spatie\Mailcoach\Domain\Audience\Support\Segments\EverySubscriberSegment',
+            ]);
+        }
+    });
+});
