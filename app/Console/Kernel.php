16edb6d18ff0272915f8dcab25a4199738d8fe59
Christoph Rumpel <c.rumpel@kabsi.at>
2021-04-15 16:57:33 +0200

    Add command to tag mp purchases in mp nl

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\AddPurchasedTagToMPSubscribers;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
 
+        AddPurchasedTagToMPSubscribers::class,
     ];
 
     /**
