1cf53ad6efe7ec866711d29f57af06d750b06d87
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-08-26 15:47:48 +0200

    Add Laravel Workflows Privacy Policy

---
+++ resources/views/pages/privacy-policy-lw.blade.php
@@ -0,0 +1,80 @@
+<x-main-layout title="Privacy Policy Mastering PhpStorm" page="privacy-policy-mp">
+    <main class="post-markdown-styles">
+        <article>
+
+            <h1>Business Privacy Policy - laravelworkflows.com</h1>
+
+            <p>This privacy policy sets out how laravelworkflows.com uses and protects any information that you give when you visit <a href="https://laravelworkflows.com/">laravelworkflows.com</a>.
+            </p>
+
+            <p>laravelworkflows.com is committed to ensuring that your privacy is protected. Should we ask you to provide
+                certain information by which you can be identified when using laravelworkflows.com, then you can be
+                assured that it will only be used in accordance with this privacy statement.</p>
+
+            <p>laravelworkflows.com may change this policy from time to time by updating this page. You should check this
+                page
+                from time to time to ensure that you are happy with any changes. This policy is effective from
+                26.08.2021.</p>
+
+            <h2>What We Collect</h2>
+
+            <p>We may collect the following information if you subscribe to the newsletter:</p>
+            <ul>
+                <li>Email address</li>
+            </ul>
+
+            <h2>What We Do with the Information We Gather</h2>
+
+            <p>We do need this information in order to:</p>
+            <ul>
+                <li>Send news about the product Laravel Workflows</li>
+            </ul>
+            <p>Who the data is shared with:</p>
+            <ul>
+                <li>The application is deployed on servers from <a href="https://www.digitalocean.com/security/gdpr/">DigitalOcean</a>
+                    and <a href="https://all-inkl.com/datenschutz/">ALL-INKL.COM</a></li>
+                <li>The email is sent through <a href="https://mailchimp.com/legal/privacy/">mailchimp</a>.
+                </li>
+            </ul>
+            <h2>How Long the Information Is Stored</h2>
+            <p>If you are only subscribed to the newsletter, the information is stored as long as you are subscribed to
+                one of the newsletters. When you end the subscription, it will automatically be deleted.</p>
+
+            <h2>Security</h2>
+
+            <p>We are committed to ensuring that your information is secure. In order to prevent unauthorised access or
+                disclosure, we have put in place suitable physical, electronic and managerial procedures to safeguard
+                and
+                secure the information we collect online.</p>
+
+            <h2>Controlling Your Personal Information</h2>
+
+            <p>You may choose to restrict the collection or use of your personal information in the following ways:</p>
+            <ul>
+                <li>Don't subscribe to one of the newsletters.</li>
+                <li>Unsubscribe from one of the newsletters. (unsubscribe link in the newsletter)</li>
+            </ul>
+
+            <p>We will not sell, distribute or lease your personal information to third parties unless we have your
+                permission or are required by law to do so.</p>
+
+            <h2>Website analytics</h2>
+            <p>We are using the privacy-focused analytics tool <a href="https://usefathom.com/">fathom</a> which is NOT using any cookies.</p>
+
+            <p>No personal data is being stored, so we only get an overview of how many people visited which site.</p>
+
+
+            <h2>Our Details</h2>
+            <p>Laravel Workflows is owned and operated by Christoph Rumpel & Sebastian Schöps.</p>
+            <p>You can contact us at: christoph@christoph-rumpel.com</p>
+
+            <p>Our business is located at:<br>
+
+                Christoph Rumpel<br/>
+                Wienergasse 39/11<br/>
+                2380 Perchtoldsdorf<br/>
+                Austria</p>
+        </article>
+
+    </main>
+</x-main-layout>
