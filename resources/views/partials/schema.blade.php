7f8f5cb228f7241c784a636f167661bb55dc6954
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-09-17 09:28:09 +0200

    Update schema info to blog

--- resources/views/partials/schema.blade.php
+++ resources/views/partials/schema.blade.php
@@ -1,6 +1,8 @@
 <?php
-echo $localBusiness = \Spatie\SchemaOrg\Schema::localBusiness()
+echo $localBusiness = \Spatie\SchemaOrg\Schema::blog()
     ->name('Christoph Rumpel')
-    ->email('christoph@christoph-rumpel.com')
+    ->description("Hey, I'm Christoph. I code. I write about code. I teach how I write code. I talk at conferences about how I teach to code.")
+    ->image(asset('images/cr.png'))
+    ->url(url('/'))
     ->toScript();
 ?>
