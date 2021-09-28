72db37d72276fa0b1757d4953e7f5a7a3a86928e
Christoph Rumpel <christoph@christoph-rumpel.com>
2021-09-28 10:01:37 +0200

    Update blog meta

--- resources/views/partials/metaPost.blade.php
+++ resources/views/partials/metaPost.blade.php
@@ -2,10 +2,10 @@
 <meta property="og:type" content="article"/>
 <meta property="og:title" content="{{ $post->title }}"/>
 <meta property="og:description" content="{{ strip_tags($post->summary) }}"/>
-<meta property="og:image" content="{{ asset('') . $post->previewImage }}"/>
+<meta property="og:image" content="{{ asset($post->previewImage) }}"/>
 
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="@christophrumpel" />
 <meta name="twitter:title" content="{{ $post->title }}" />
 <meta name="twitter:description" content="{{ strip_tags($post->summary) }}" />
-<meta name="twitter:image" content="{{ asset('') . $post->previewImageTwitter ?? $post->previewImage }}" />
+<meta name="twitter:image" content="{{ asset($post->previewImageTwitter ?? $post->previewImage) }}" />
