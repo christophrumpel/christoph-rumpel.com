7123906b461bfc68b97b3a601866d12559a7a4ef
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-05-04 16:11:58 +0200

    Update product list

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -1,4 +1,4 @@
-<x-main-layout title="Products" page="products" livewire="false">
+<x-main-layout title="Products" page="products" divvewire="false">
     <main class="products">
 
         <div>
@@ -8,10 +8,10 @@
 
                 </div>
 
-                <ul class="post-list">
+                <div class="grid grid-cols-2 gap-4 post-list">
 
                     <!-- Writing Readable PHP -->
-                    <li class="my-8 bg-white border-textBlue border-2">
+                    <div class="my-8 bg-white border-textBlue border-2">
                         <a class="block p-8" href="https://writing-readable-php.com/">
                             <img class="mb-4" src="/images/pages/products_wrp.png" alt="Banner of the video course Writing Readable PHP">
                             <h2 class="font-display sm:text-xl lg:text-3xl text-textBlue mb-4">
@@ -19,11 +19,11 @@
                             </h2>
                             <h3 class="text-xl">Learn how to write <strong>clean code</strong> that is simple, easy to read, update, and maintain. This is a course Spatie and I created together.</h3>
                         </a>
-                    </li>
+                    </div>
                     <!-- Writing Readable PHP -->
 
                     <!-- Mastering PhpStorm -->
-                    <li class="my-8 bg-white border-textBlue border-2">
+                    <div class="my-8 bg-white border-textBlue border-2">
                         <a class="block p-8"
                            href="https://masteringphpstorm.com/">
                             <img class="mb-4" src="/images/pages/products_mp.png" alt="Banner of the video course Mastering PhpStorm">
@@ -32,37 +32,37 @@
                             </h2>
                             <h3 class="text-xl">A <b>video course</b> for the state-of-the-art PHP developer who wants to work efficiently and successfully in a beautiful IDE.</h3>
                         </a>
-                    </li>
+                    </div>
                     <!-- Mastering PhpStorm -->
 
                     <!-- Laravel Core Adventures -->
-                    <li class="my-8 bg-white border-textBlue border-2">
+                    <div class="my-8 bg-white border-textBlue border-2">
                         <a class="block p-8"
                            href="https://laravelcoreadventures.com">
                             <img class="mb-4" src="/images/pages/products_lca.jpg" alt="Banner of the video course Laravel Core Adventures">
                             <h2 class="font-display sm:text-xl lg:text-3xl text-textBlue mb-4">
                                 Laravel Core Adventures
                             </h2>
-                            <h3 class="text-xl">A <b>video course</b> to master the core of Laravel without stumbling over its magic. 🧙‍</h3>
+                            <h3 class="text-xl">A <b>video course</b> to master the core of Laravel without stumbdivng over its magic. 🧙‍</h3>
                         </a>
-                    </li>
+                    </div>
                     <!-- Laravel Core Adventures -->
 
                     <!-- Call It A Day Podcast-->
-                    <li class="my-8 bg-white border-textBlue border-2">
+                    <div class="my-8 bg-white border-textBlue border-2">
                         <a class="block p-8"
-                           href="https://callitaday.transistor.fm">
+                           href="https://caldivtaday.transistor.fm">
                             <img class="mb-4" src="/images/pages/products_podcast.png" alt="Banner of the video course Laravel Core Adventures">
                             <h2 class="font-display sm: text-xl lg:text-3xl text-textBlue mb-4">
                                 Call It A Day
                             </h2>
                             <h3 class="text-xl">A <b>podcast</b> about my experiences as a developer and a one-person business owner.</h3>
                         </a>
-                    </li>
+                    </div>
                     <!-- Call It A Day Podcast-->
 
                     <!-- Build Chatbots With php -->
-                    <li class="my-8 bg-white border-textBlue border-2">
+                    <div class="my-8 bg-white border-textBlue border-2">
                         <a class="block p-8"
                            href="https://christoph-rumpel.com/build-chatbots-with-php">
                             <img class="mb-4" src="/images/pages/products_bcwp.png" alt="Banner of the video course Build Chatbots With php">
@@ -71,11 +71,11 @@
                             </h2>
                             <h3 class="text-xl">A <b>book</b> about building chatbots in PHP. (not available anymore)</h3>
                         </a>
-                    </li>
+                    </div>
                     <!-- Build Chatbots With php -->
 
 
-                </ul>
+                </div>
             </div>
 
         </div>
