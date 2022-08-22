e66cfb0af49a98a194e4bdbb593bbb6cc0a3abb4
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-08-22 17:22:48 +0200

    Remove call to google for font Bitter

--- resources/views/partials/fontStyles.blade.php
+++ resources/views/partials/fontStyles.blade.php
@@ -1,38 +1,42 @@
 <style>
-    /* latin-ext */
+    /* bitter-regular - latin */
     @font-face {
         font-family: 'Bitter';
         font-style: normal;
         font-weight: 400;
-        font-display: swap;
-        src: local('Bitter Regular'), local('Bitter-Regular'), url(https://fonts.gstatic.com/s/bitter/v15/rax8HiqOu8IVPmn7cYxpLjpSm3LZ.woff2) format('woff2');
-        unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+        src: url('../fonts/bitter-v28-latin-regular.eot'); /* IE9 Compat Modes */
+        src: local(''),
+        url('../fonts/bitter-v28-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+        url('../fonts/bitter-v28-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+        url('../fonts/bitter-v28-latin-regular.woff') format('woff'), /* Modern Browsers */
+        url('../fonts/bitter-v28-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+        url('../fonts/bitter-v28-latin-regular.svg#Bitter') format('svg'); /* Legacy iOS */
     }
-    /* latin */
+
     @font-face {
         font-family: 'Bitter';
         font-style: normal;
-        font-weight: 400;
-        font-display: swap;
-        src: local('Bitter Regular'), local('Bitter-Regular'), url(https://fonts.gstatic.com/s/bitter/v15/rax8HiqOu8IVPmn7f4xpLjpSmw.woff2) format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        font-weight: 600;
+        src: url('../fonts/bitter-v28-latin-regular.eot'); /* IE9 Compat Modes */
+        src: local(''),
+        url('../fonts/bitter-v28-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+        url('../fonts/bitter-v28-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+        url('../fonts/bitter-v28-latin-regular.woff') format('woff'), /* Modern Browsers */
+        url('../fonts/bitter-v28-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+        url('../fonts/bitter-v28-latin-regular.svg#Bitter') format('svg'); /* Legacy iOS */
     }
-    /* latin-ext */
+
+    /* bitter-700 - latin */
     @font-face {
         font-family: 'Bitter';
         font-style: normal;
         font-weight: 700;
-        font-display: swap;
-        src: local('Bitter Bold'), local('Bitter-Bold'), url(https://fonts.gstatic.com/s/bitter/v15/rax_HiqOu8IVPmnzxKl8DRhfsUjQ8Qad.woff2) format('woff2');
-        unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-    }
-    /* latin */
-    @font-face {
-        font-family: 'Bitter';
-        font-style: normal;
-        font-weight: 700;
-        font-display: swap;
-        src: local('Bitter Bold'), local('Bitter-Bold'), url(https://fonts.gstatic.com/s/bitter/v15/rax_HiqOu8IVPmnzxKl8AxhfsUjQ8Q.woff2) format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        src: url('../fonts/bitter-v28-latin-700.eot'); /* IE9 Compat Modes */
+        src: local(''),
+        url('../fonts/bitter-v28-latin-700.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+        url('../fonts/bitter-v28-latin-700.woff2') format('woff2'), /* Super Modern Browsers */
+        url('../fonts/bitter-v28-latin-700.woff') format('woff'), /* Modern Browsers */
+        url('../fonts/bitter-v28-latin-700.ttf') format('truetype'), /* Safari, Android, iOS */
+        url('../fonts/bitter-v28-latin-700.svg#Bitter') format('svg'); /* Legacy iOS */
     }
 </style>
