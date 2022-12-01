e8c62672e06a4f570a3a17c9725e45bd4c220191
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-12-01 16:04:06 +0100

    Update uses meta tags

--- resources/views/partials/metaUses.blade.php
+++ resources/views/partials/metaUses.blade.php
@@ -8,4 +8,4 @@
 <meta name="twitter:site" content="@christophrumpel" />
 <meta name="twitter:title" content="The Tools That Help Me Create" />
 <meta name="twitter:description" content="This is my current development setup which includes all the hard- and software I need for my daily work as a web developer and content creator." />
-<meta name="twitter:image" content="{{ asset('images/pages/uses_desk_side.jpg') }}" />
+<meta name="twitter:image" content="{{ asset('images/pages/uses_desk_main.jpg') }}" />
