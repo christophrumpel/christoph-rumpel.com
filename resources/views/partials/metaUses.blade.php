3bca23ca895c670190c4bdaae45c115c09e9ec67
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-12-01 16:07:06 +0100

    Update uses meta tags

--- resources/views/partials/metaUses.blade.php
+++ resources/views/partials/metaUses.blade.php
@@ -2,7 +2,7 @@
 <meta property="og:type" content="article"/>
 <meta property="og:title" content="The Tools That Help Me Create"/>
 <meta property="og:description" content="This is my current development setup which includes all the hard- and software I need for my daily work as a web developer and content creator."/>
-<meta property="og:image" content="{{ asset('images/pages/uses_desk_side.jpg') }}"/>
+<meta property="og:image" content="{{ asset('images/pages/uses_desk_main.jpg') }}"/>
 
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="@christophrumpel" />
