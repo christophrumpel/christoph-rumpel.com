ad61046cd9ff8cc0ba47eaf8ec7c1651a5777cbb
Christoph Rumpel <christoph@christoph-rumpel.com>
2023-03-03 15:58:56 +0100

    remove foreign key check for mailcoach deletion

--- database/migrations/2023_03_03_152127_remove_mailcoach_tables.php
+++ database/migrations/2023_03_03_152127_remove_mailcoach_tables.php
@@ -6,12 +6,14 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        // Delete all tables starting with Mailcoach
+        // Delete all tables starting with Mailcoach force it even for relations
         $tables = Schema::getConnection()->getDoctrineSchemaManager()->listTableNames();
+        Schema::disableForeignKeyConstraints();
         foreach ($tables as $table) {
             if (str_starts_with($table, 'mailcoach')) {
                 Schema::dropIfExists($table);
             }
         }
+        Schema::enableForeignKeyConstraints();
     }
 };
