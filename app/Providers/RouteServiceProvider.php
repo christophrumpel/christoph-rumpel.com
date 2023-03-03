83d809f9411e19c49620daa852c7f8faa66afe06
Shift <shift@laravelshift.com>
2023-03-03 07:44:30 +0000

    Adopt class based routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,7 +65,6 @@ class RouteServiceProvider extends ServiceProvider
             ->group(base_path('routes/web.php'));
 
         Route::middleware('web')
-            ->namespace($this->namespace)
             ->group(base_path('routes/privacy.php'));
     }
 
