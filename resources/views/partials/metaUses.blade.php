eeba1820f2b0e66bd2d654fb94a3cf05fa410326
Christoph Rumpel <christoph@christoph-rumpel.com>
2024-09-25 16:49:56 +0200

    Update meta pic for uses page

--- resources/views/partials/metaUses.blade.php
+++ resources/views/partials/metaUses.blade.php
@@ -2,10 +2,10 @@
 <meta property="og:type" content="article"/>
 <meta property="og:title" content="The Tools That Help Me Create"/>
 <meta property="og:description" content="This is my current development setup which includes all the hard- and software I need for my daily work as a web developer and content creator."/>
-<meta property="og:image" content="{{ asset('images/pages/uses_desk_main.jpg') }}"/>
+<meta property="og:image" content="{{ asset('images/pages/uses_my_setup.png') }}"/>
 
 <meta name="twitter:card" content="summary_large_image" />
 <meta name="twitter:site" content="@christophrumpel" />
 <meta name="twitter:title" content="The Tools That Help Me Create" />
 <meta name="twitter:description" content="This is my current development setup which includes all the hard- and software I need for my daily work as a web developer and content creator." />
-<meta name="twitter:image" content="{{ asset('images/pages/uses_desk_main.jpg') }}" />
+<meta name="twitter:image" content="{{ asset('images/pages/uses_my_setup.png') }}" />
