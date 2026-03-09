5759f59dd5f230d557c97ba0a30e88f0a649d36f
jarvy-cr <jarvy@rumpel.co.at>
2026-03-09 12:23:32 +0000

    Remove stray character from post template

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -1,4 +1,4 @@
-^<x-main-layout :post="$post" :title="$post->title" page="post">
+<x-main-layout :post="$post" :title="$post->title" page="post">
     <main class="mt-16">
 
         <article class="post">
