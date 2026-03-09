c11dd96d587abadfe4b503f8480e83b1e4f7a663
jarvy-cr <jarvy@rumpel.co.at>
2026-03-09 12:07:48 +0000

    Fix font paths to use absolute URLs
    Relative paths (../fonts/) resolved incorrectly on article pages
    like /2026/the-agentic-artisan, causing 404s for all font files.

--- resources/views/partials/fontStyles.blade.php
+++ resources/views/partials/fontStyles.blade.php
@@ -9,13 +9,13 @@
         font-style: normal;
         font-weight: 400;
         font-display: swap;
-        src: url('../fonts/bitter-v28-latin-regular.eot'); /* IE9 Compat Modes */
+        src: url('/fonts/bitter-v28-latin-regular.eot'); /* IE9 Compat Modes */
         src: local(''),
-        url('../fonts/bitter-v28-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-        url('../fonts/bitter-v28-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
-        url('../fonts/bitter-v28-latin-regular.woff') format('woff'), /* Modern Browsers */
-        url('../fonts/bitter-v28-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
-        url('../fonts/bitter-v28-latin-regular.svg#Bitter') format('svg'); /* Legacy iOS */
+        url('/fonts/bitter-v28-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+        url('/fonts/bitter-v28-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+        url('/fonts/bitter-v28-latin-regular.woff') format('woff'), /* Modern Browsers */
+        url('/fonts/bitter-v28-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+        url('/fonts/bitter-v28-latin-regular.svg#Bitter') format('svg'); /* Legacy iOS */
     }
 
     @font-face {
@@ -23,13 +23,13 @@
         font-style: normal;
         font-weight: 600;
         font-display: swap;
-        src: url('../fonts/bitter-v28-latin-regular.eot'); /* IE9 Compat Modes */
+        src: url('/fonts/bitter-v28-latin-regular.eot'); /* IE9 Compat Modes */
         src: local(''),
-        url('../fonts/bitter-v28-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-        url('../fonts/bitter-v28-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
-        url('../fonts/bitter-v28-latin-regular.woff') format('woff'), /* Modern Browsers */
-        url('../fonts/bitter-v28-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
-        url('../fonts/bitter-v28-latin-regular.svg#Bitter') format('svg'); /* Legacy iOS */
+        url('/fonts/bitter-v28-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+        url('/fonts/bitter-v28-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+        url('/fonts/bitter-v28-latin-regular.woff') format('woff'), /* Modern Browsers */
+        url('/fonts/bitter-v28-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+        url('/fonts/bitter-v28-latin-regular.svg#Bitter') format('svg'); /* Legacy iOS */
     }
 
     /* bitter-700 - latin */
@@ -38,12 +38,12 @@
         font-style: normal;
         font-weight: 700;
         font-display: swap;
-        src: url('../fonts/bitter-v28-latin-700.eot'); /* IE9 Compat Modes */
+        src: url('/fonts/bitter-v28-latin-700.eot'); /* IE9 Compat Modes */
         src: local(''),
-        url('../fonts/bitter-v28-latin-700.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-        url('../fonts/bitter-v28-latin-700.woff2') format('woff2'), /* Super Modern Browsers */
-        url('../fonts/bitter-v28-latin-700.woff') format('woff'), /* Modern Browsers */
-        url('../fonts/bitter-v28-latin-700.ttf') format('truetype'), /* Safari, Android, iOS */
-        url('../fonts/bitter-v28-latin-700.svg#Bitter') format('svg'); /* Legacy iOS */
+        url('/fonts/bitter-v28-latin-700.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+        url('/fonts/bitter-v28-latin-700.woff2') format('woff2'), /* Super Modern Browsers */
+        url('/fonts/bitter-v28-latin-700.woff') format('woff'), /* Modern Browsers */
+        url('/fonts/bitter-v28-latin-700.ttf') format('truetype'), /* Safari, Android, iOS */
+        url('/fonts/bitter-v28-latin-700.svg#Bitter') format('svg'); /* Legacy iOS */
     }
 </style>
